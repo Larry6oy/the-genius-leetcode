559b0e366ec7b3e2137ffeedd485821597e41f1a
larry6oy <peng.larry@outlook.com>
2021-12-22 19:23:18 -0500

    leetcode 217

---
+++ src/leetcode_217.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return True;
+
+
+example = Solution()
+
+print(example.containsDuplicate([1,1]))
