bd0ea33dc82db02fb909982cfeb78e8d83f8a721
larry6oy <peng.larry@outlook.com>
2022-04-07 12:37:55 -0400

    cool

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,34 +1,46 @@
+from random import randrange
 from typing import List
 
 
 class Solution:
     def partition(self, nums: List[int], start : int, end: int) -> List[int]:
-        p = end
+
+        if start >= end:
+            return
+
+        p = randrange(0, end)
         j = start
         i = start-1
+
+
         while j < end:
-            if nums[j] < nums[p]:
+            if nums[j] <= nums[p]:
                 i = i+1
                 temp = nums[i]
                 nums[i] = nums[j]
                 nums[j] = temp
+
             j = j+1
         temp = nums[p]
         nums[p] = nums[i+1]
         nums[i+1] = temp
-        while p < start:
-            self.partition(nums, start, p)
-        while p < end:
-            self.partition(nums, p, end)
-        return nums
+        p = i+1
+        self.partition(nums, start, p-1)
+        self.partition(nums, p+1, end)
+
     def quickSort(self, nums: List[int]) -> List[int]:
-       self.partition(nums, 0, len(nums)-1)
-       return nums
+        if len(nums) > 0:
+            self.partition(nums, 0, len(nums)-1)
+        return nums
 
 example = Solution()       
 
 
 
 
-print(example.partition([10, 80, 30, 90, 40, 50, 70], 0, 6)) # => [10, 30, 40, 50, 70, 90, 80]
+#print(example.partition([10, 80, 30, 90, 40, 50, 70], 0, 6)) # => [10, 30, 40, 50, 70, 80, 90]
 print(example.quickSort([10, 80, 30, 90, 40, 50, 70])) # => [10, 30, 40, 50, 70, 80, 90]
+print(example.quickSort([])) # => []
+print(example.quickSort([3])) # => [3]
+print(example.quickSort([3, 1, 3, 40, 5])) # => [1, 3, 3, 5, 40]
+print(example.quickSort([3, 1, 3])) # => [1, 3]
