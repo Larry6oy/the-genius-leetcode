5b67ef0f38163529d019ee5b033a2c32aee7bb85
larry6oy <peng.larry@outlook.com>
2022-04-19 19:34:51 -0400

    The Commit

--- src/linklist/LinkList.py
+++ src/linklist/LinkList.py
@@ -1,3 +1,4 @@
+
 from typing import List
 
 #definition
@@ -9,22 +10,37 @@ class Node:
 
   def print(self):
     print("Hello my value is", self.value)
-    
+
 class LinkList:
   def __init__(self, head: Node, tail: Node):
     self.tail = tail
     self.head = head
 
   def print(self):
-    print( 'Hello!  I am a list.  My head is', self.head.value, ', my tail is', self.tail.value)
+
+    p = self.head
+    strs = [ ]
+    while p != None:
+        strs.append(str(p.value))
+        p = p.next
+        
+    print( 'Hello!  I am a list.  My values are', ' -> '.join(strs))
 
 # execution
 p1 = Node(4, None, None)
 p2 = Node(2, p1, None)
+p3 = Node(3, p2, None)
+p4 = Node(1, p3, None)
+p5 = Node(9, p4, None)
+
 p1.next = p2
+p2.next = p3
+p3.next = p4
+p4.next = p5
+
+
 p1.print()
 p2.print()
-
-l1= LinkList(p1, p2)
+l1= LinkList(p1, p5)
 l1.print()
 # l1.print() # 4 -> 2
