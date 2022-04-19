f2d19328f3ed9190667e3a7771b0a047a3fbe2ec
larry6oy <peng.larry@outlook.com>
2022-04-19 19:58:32 -0400

    The Commit 2

--- src/linklist/LinkList.py
+++ src/linklist/LinkList.py
@@ -3,21 +3,20 @@ from typing import List
 
 #definition
 class Node:
-  def __init__(self, value: int, prev, next):
+  def __init__(self, value: int):
     self.value = value
-    self.prev = prev
-    self.next = next
+    self.prev = None
+    self.next = None
 
   def print(self):
     print("Hello my value is", self.value)
 
 class LinkList:
-  def __init__(self, head: Node, tail: Node):
-    self.tail = tail
-    self.head = head
+  def __init__(self, nodes: List[Node]):
+    self.tail = nodes[len(nodes)-1]
+    self.head = nodes[0]
 
   def print(self):
-
     p = self.head
     strs = [ ]
     while p != None:
@@ -27,20 +26,14 @@ class LinkList:
     print( 'Hello!  I am a list.  My values are', ' -> '.join(strs))
 
 # execution
-p1 = Node(4, None, None)
-p2 = Node(2, p1, None)
-p3 = Node(3, p2, None)
-p4 = Node(1, p3, None)
-p5 = Node(9, p4, None)
-
-p1.next = p2
-p2.next = p3
-p3.next = p4
-p4.next = p5
+p1 = Node(4)
+p2 = Node(2)
+p3 = Node(3)
+p4 = Node(1)
+p5 = Node(9)
 
 
-p1.print()
-p2.print()
-l1= LinkList(p1, p5)
+
+l1= LinkList([p1, p2, p3, p4, p5])
 l1.print()
 # l1.print() # 4 -> 2
