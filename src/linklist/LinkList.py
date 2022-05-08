03790b1b730e90a7b6f6313e81daa023a8b80f79
larry6oy <peng.larry@outlook.com>
2022-05-08 13:07:23 -0400

    Code Complete

--- src/linklist/LinkList.py
+++ src/linklist/LinkList.py
@@ -1,4 +1,5 @@
 
+from multiprocessing.sharedctypes import Value
 from typing import List
 
 #definition
@@ -26,10 +27,30 @@ class LinkList:
             n2.prev = n1
             i = i+1
 
-#    def reverse(self, nodes: List(Node)):
- #       a = 0
-#        z = len(nodes)-1
+    def getval(self, index: int):
+        if index < 0:
+            return '我不会， 长大后在学习！'
+            
+        curr = self.head
+        i = 0
+        while i<index and curr.next != None:
+            curr = curr.next
+            i = i+1
+        if index > i:
+            curr.value = '我不会， 长大后在学习！'
 
+        return curr.value
+        
+
+    def reverse(self, nodes: List[Node]):
+        a = 0
+        z = len(nodes)-1
+        while a < z:
+            temp = nodes[a]
+            nodes[a] = nodes[z]
+            nodes[z] = temp
+            a = a+1
+            z = z-1
 
     def print(self):
         p = self.head
@@ -52,4 +73,11 @@ p5 = Node(9)
 #l1= LinkList([p1, p2, p3, p4, p5])
 l1 = LinkList([4, 2, 3, 1, 9])
 l1.print()
-# l1.print() # 4 -> 2
+print(l1.getval(4))
+print(l1.getval(0))
+print(l1.getval(5))
+print(l1.getval(3))
+print(l1.getval(6))
+print(l1.getval(-1))
+
+#[9, 1, 3, 2, 4]
