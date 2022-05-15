479fbbfd968d74d1b112dc7969ea214e4abea8a7
larry6oy <peng.larry@outlook.com>
2022-05-15 09:24:25 -0400

    Quick Save

--- src/linklist/LinkList.py
+++ src/linklist/LinkList.py
@@ -30,7 +30,7 @@ class LinkList:
     def getval(self, index: int):
         if index < 0:
             return '我不会， 长大后在学习！'
-            
+
         curr = self.head
         i = 0
         while i<index and curr.next != None:
@@ -42,15 +42,15 @@ class LinkList:
         return curr.value
         
 
-    def reverse(self, nodes: List[Node]):
-        a = 0
-        z = len(nodes)-1
-        while a < z:
-            temp = nodes[a]
-            nodes[a] = nodes[z]
-            nodes[z] = temp
-            a = a+1
-            z = z-1
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.head.prev = None
+        self.tail.next = None
+
 
     def print(self):
         p = self.head
@@ -62,22 +62,27 @@ class LinkList:
         print( 'Hello!  I am a list.  My values are', ' -> '.join(strs))
 
 # execution
-p1 = Node(4)
-p2 = Node(2)
-p3 = Node(3)
-p4 = Node(1)
-p5 = Node(9)
+#p1 = Node(4)
+#p2 = Node(2)
+#p3 = Node(3)
+#p4 = Node(1)
+#p5 = Node(9)
+pp1 = Node(1)
+pp2 = Node(3)
+pp3 = Node(2)
 
 
 
 #l1= LinkList([p1, p2, p3, p4, p5])
-l1 = LinkList([4, 2, 3, 1, 9])
+l1 = LinkList([1, 3, 2])
 l1.print()
-print(l1.getval(4))
-print(l1.getval(0))
-print(l1.getval(5))
-print(l1.getval(3))
-print(l1.getval(6))
-print(l1.getval(-1))
+#print(l1.getval(4))
+#print(l1.getval(0))
+#print(l1.getval(5))
+#print(l1.getval(3))
+#print(l1.getval(6))
+#print(l1.getval(-1))
+l1.reverse()
+l1.print()
 
 #[9, 1, 3, 2, 4]
