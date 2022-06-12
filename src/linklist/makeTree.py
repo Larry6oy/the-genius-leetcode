56ddd272e7648d593bb45fe8714f4c765991cfad
larry6oy <peng.larry@outlook.com>
2022-06-12 15:37:51 -0400

    print(tree) succsess

--- src/linklist/makeTree.py
+++ src/linklist/makeTree.py
@@ -6,15 +6,31 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+    def toString(self):
+        if self.left == None and self.right == None:
+            r = str(self.value)
+        else:
+            r = '('
+            r = r+ str(self.value)
+            r = r+ ', '
+            if self.left != None:  
+                r = r+str(self.left.toString())
+            else:
+                r = r+ str('')
+            r = r+', '
+            if self.right != None:
+                r = r+str(self.right.toString())
+            else:
+                r = r+str('')
+            r = r+')'
+        return r
+
+
 class Tree:
     def __init__(self, value: Node):
         self.root = value
     def toString(self):
-        r = '('
-        r = r+'1, '
-        r = r+'2, '
-        r = r+'3'
-        r = r+')'
+        r = self.root.toString()
         return r
     def print(self):
         string = []
@@ -54,11 +70,15 @@ def makeSimpleTree():
     n1 = Node(1)
     n2 = Node(2)
     n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
     n1.left = n2
     n1.right = n3
+    n2.left = n4
+    n2.right = n5
     tr = Tree(n1)
     return tr
-t = makeSimpleTree()
+t = makeTree()
 print(t.toString())
 #print(makeTree())
-print(t)
+#print(t.value)
